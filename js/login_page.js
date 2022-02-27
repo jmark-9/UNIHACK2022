c57d5b8ce3d91594a9b02a72c1d774140448d55d
jmark-9 <jesskymark@gmail.com>
2022-02-27 11:59:06 +1100

    more login page stuff

---
+++ js/login_page.js
@@ -0,0 +1,20 @@
+`<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+<input class="mdl-textfield__input" type="text" id="registerUsername">
+<label class="mdl-textfield__label" for="registerUsername">Username...</label>
+</div>
+
+<br />
+
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+<input class="mdl-textfield__input" type="password" id="registerPassword">
+<label class="mdl-textfield__label" for="registerPassword">Password...</label>
+</div>
+
+<br />
+
+<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+<input class="mdl-textfield__input" type="password" id="confirmPassword">
+<label class="mdl-textfield__label" for="confirmPassword">Confirm password...</label>
+</div>
+
+<br />`
