254bbd4556e595ac556c14c5d0fea798c8dbe002
Alexander Authors <alex1authors@gmail.com>
2022-02-27 16:59:04 +1100

    did some ui for outdoors

--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@
 
 function onLoad() {
     let message = document.getElementById("greeting");
-    message.innerText = `Welcome Bestie ${currentAccount._name}!`;
+    message.innerText = `Welcome ${currentAccount._name}!`;
 }
 
 function checkNotifs() {
     // Let's check if the browser supports notifications
-  
+
     // Let's check whether notification permissions have already been granted
     if (Notification.permission === "granted") {
         // do nothing
     }
-  
+
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== "denied") {
-      Notification.requestPermission()
+        Notification.requestPermission()
     }
-  
+
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them any more.
 }
